b3d051a79277b03848d5e6f8420ef05c26e107d7
IIIAuPMa <kalash1605@mail.ru>
2025-05-12 15:18:36 +0700

    Add files via upload

---
+++ Zones/Surface.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LabApi.Features.Wrappers;
+using MapGeneration;
+
+namespace Surface
+{
+    public class SurfaceZone
+    {
+        public void SRed()
+        {
+
+            Map.SetColorOfLights(UnityEngine.Color.red,FacilityZone.Surface);
+
+        }
+        public void SBlue()
+        {
+
+            Map.SetColorOfLights(UnityEngine.Color.blue, FacilityZone.Surface);
+
+        }
+        public void SGreen()
+        {
+
+            Map.SetColorOfLights(UnityEngine.Color.green, FacilityZone.Surface);
+
+        }
+        public void SBlack()
+        {
+
+            Map.SetColorOfLights(UnityEngine.Color.black, FacilityZone.Surface);
+
+        }
+        public void SCyan()
+        {
+
+            Map.SetColorOfLights(UnityEngine.Color.cyan, FacilityZone.Surface);
+
+        }
+        public void SYellow()
+        {
+
+            Map.SetColorOfLights(UnityEngine.Color.yellow, FacilityZone.Surface);
+
+        }
+        public void SPurple()
+        {
+
+            Map.SetColorOfLights(UnityEngine.Color.magenta, FacilityZone.Surface);
+
+        }
+    }
+}
